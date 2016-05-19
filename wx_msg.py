ea9a64b5ca0b1a0011fdaf4b64d2e4440f82df8b
ETY001 <ety001@domyself.me>
2016-05-19 15:19:27 +0800

    完工

--- wx_msg.py
+++ wx_msg.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
-import urllib, urllib2, json, os, time
+import urllib, urllib2, json, os, time, sys
 
+# You need to edit from here
 tmpl_id = 'PT0X_93niu22Ti3CYqEL0bXPkflJ06zUK5Yt3_KCF_g'
 appid = 'wx54602a12c477c961'
 secret = 'dde1ceb8fb98cb3ec0a70e9f0849221b'
 user_id = 'oUeGNtz41L35y49a_xqXGjWeBazU'
+
+tmpl_data = {
+    'touser':user_id,
+    'template_id':tmpl_id,
+    'url': 'http://www.domyself.me',
+    'topcolor': '#ff0000',
+    'data': {
+        'first': {
+            'value': '',
+            'color': '#173177'
+        },
+        'remark': {
+            'value': '',
+            'color': '#173177'
+        }
+    }
+}
+# You need to edit end this
 
 access_url = 'https://api.weixin.qq.com/cgi-bin/token'
 access_url += '?grant_type=client_credential&appid='
@@ -43,4 +62,19 @@ def get_access_token():
         f.close()
     return access_token
 
-print get_access_token()
+def get_tmpl_url():
+    return tmpl_url + get_access_token();
+
+def send(params):
+    param_arr = params.split('|')
+    i = 0
+    for k in tmpl_data['data']:
+        tmpl_data['data'][k]['value'] = param_arr[i]
+        i = i + 1
+    request = urllib2.Request(get_tmpl_url() , json.dumps(tmpl_data))
+    response=urllib2.urlopen(request)
+
+if len(sys.argv) == 3:
+    user_id = sys.argv[2]
+if len(sys.argv) == 2:
+    send(sys.argv[1])
